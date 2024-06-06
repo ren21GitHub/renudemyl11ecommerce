0cf8dab76962fcb9f5dda20f4daa6aa4a9119bb4
ren21GitHub <reynielobut21@gmail.com>
2024-06-06 18:30:39 +0800

    Frontend Template Setup

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,12 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\UserDashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -26,3 +22,17 @@ require __DIR__.'/auth.php';
 // require __DIR__.'/vendor.php';
 
 Route::get('admin/login', [AdminController::class, 'login'])->middleware('guest')->name('admin.login');
+
+
+// Route::get('/dashboard', function () {
+//     return view('frontend.dashboard.dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::group(['middleware' => ['auth', 'verified'],
+               'prefix' => 'user',
+               'name' => 'user.'             
+            ], function(){
+                Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+            });
